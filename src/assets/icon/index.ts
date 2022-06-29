63d3a1fc4f450575ec212b73da966d896fdb6bae
Wilfredo Huancollo <wilfredo_sj@hotmail.com>
2022-06-29 09:56:16 -0500

    feat: add icons and images

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,8 @@
+export { ReactComponent as MenuIcon } from "./menu.svg"
+export { ReactComponent as MoreIcon } from "./more.svg"
+export { ReactComponent as NextIcon } from "./next.svg"
+export { ReactComponent as PauseIcon } from "./pause.svg"
+export { ReactComponent as PlayIcon } from "./play.svg"
+export { ReactComponent as PrevIcon } from "./prev.svg"
+export { ReactComponent as SearchIcon } from "./search.svg"
+export { ReactComponent as SoundIcon } from "./sound.svg"
